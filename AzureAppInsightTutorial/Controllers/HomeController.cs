5e6da01ba0a3a86ea096ae32f4e4ac16b6cb9ef4
Ramon Cruz <ramon26cruz@hotmail.com>
2018-01-26 13:30:48 +0800

    some message

---
+++ AzureAppInsightTutorial/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
+
+namespace AzureAppInsightTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TelemetryClient _telemetry;
+        public HomeController()
+        {
+            _telemetry = new TelemetryClient();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.SampleVariable = ConfigurationManager.AppSettings["SampleVariable"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            throw new Exception("Test");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Track()
+        {
+            _telemetry.TrackEvent("Win");
+            return RedirectToAction("Index");
+        }
+    }
+}
